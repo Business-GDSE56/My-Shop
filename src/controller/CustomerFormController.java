c18dac9f9a71632ff1359111c7aae73f0426c263
Charith-Darshana <darshanacharith@gmail.com>
2021-10-09 11:18:07 +0530

    Cutomer Form 1st Update

---
+++ src/controller/CustomerFormController.java
@@ -0,0 +1,16 @@
+/*
+ * ---------------------------------------------------------------------
+ *  Copyright (c) CDM-SW-SYSTEMS. All rights reserved.
+ *  Licensed under the SriLankan Information License.
+ *  See LICENSE .txt in the project root for license. information.
+ * ----------------------------------------------------------------------
+ */
+
+package controller;
+
+/**
+ * @author Charith Darshana <darshanacharith4@gmail.com>
+ * @since 2021-10-09 11:13 AM
+ */
+public class CustomerFormController {
+}
