3fd14681199cb36e3dc022816489419f46e981aa
Charith-Darshana <darshanacharith@gmail.com>
2021-10-09 09:12:59 +0530

    Initial Project

---
+++ README.md
@@ -0,0 +1,5 @@
+How to Add Videos
+    
+    1. Click Add+ Button
+    2. Select Videos 
+    3. Click Save Button
---
+++ src/AppInitializer.java
@@ -0,0 +1,27 @@
+/*
+ * ---------------------------------------------------------------------
+ *  Copyright (c) CDM-SW-SYSTEMS. All rights reserved.
+ *  Licensed under the SriLankan Information License.
+ *  See LICENSE .txt in the project root for license. information.
+ * ----------------------------------------------------------------------
+ */
+
+/**
+ * @author Charith Darshana <darshanacharith4@gmail.com>
+ * @since 2021-10-09 9:08 AM
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
